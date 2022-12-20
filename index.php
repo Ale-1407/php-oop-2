8a3366af5429e5519ae7049f35cecf98ee543955
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-20 10:47:59 +0100

    stampa card

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ include_once __DIR__ . './classi/cibo.php';
 include_once __DIR__ . './classi/giocattoli.php';
 
 $categorie = [
-    'cane' => new Categoria('cane', '<i class="fa-solid fa-dog"></i>'),
-    'gatto' => new Categoria('gatto', '<i class="fa-solid fa-cat"></i>'),
-    'uccello' => new Categoria('uccello', '<i class="fa-solid fa-crow"></i>')
+    'cane' => new Category('cane', '<i class="fa-solid fa-dog"></i>'),
+    'gatto' => new Category('gatto', '<i class="fa-solid fa-cat"></i>'),
+    'uccello' => new Category('uccello', '<i class="fa-solid fa-crow"></i>')
 ];
 
 $prodotto = [
@@ -18,7 +18,9 @@ $prodotto = [
     new Giocattoli('https://picsum.photos/800/1200', 'Topini di peluche', 9.99, $categorie['gatto'], 'morbido ed intrattenente', '8.5 x 10')
 ];
 
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,7 +32,31 @@ $prodotto = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    
+    <h1>Pet Shop</h1>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach( $prodotto as $elem ){ ?>
+              <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="<?php echo $elem->immagine ?>" class="card-img-top" alt="<?php echo $elem->nome ?>" >
+                  <div class="card-body">
+                    <h5 class="card-title"><?php echo $elem->nome ?></h5>
+                    
+                    
+                  </div>
+                </div>
+              </div>
+           <?php } ?>
+            
+        </div>
+
+    </div>
+
+
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
